fd9625bfc1c71d27602bac80520a6e797eb88a59
seemabibi <seema.bibi@hotmail.co.uk>
2023-03-05 20:34:24 +0000

    changed error on count

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+      },
+      message: props => `${props.value} is not a valid e-mail!`
+    },
+    required: [true, 'User e-mail required'],
+    unique: true
+  },
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    },
+  ],
+},
+{
+  toJSON: {
+    virtuals: true
+  }
+});
+
+userSchema.virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  });
+
+const User = model('User', userSchema);
+
+module.exports = User;
